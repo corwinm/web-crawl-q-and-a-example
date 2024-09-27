451379eef5af753a386bd4e9f3ad8b1eb8e534cc
Corwin Marsh <corwinm@users.noreply.github.com>
2024-09-27 13:49:02 -0700

    Updates to get running with python 3.9

---
+++ embeddings_utils.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from scipy import spatial
+
+
+def distances_from_embeddings(
+    query_embedding: List[float],
+    embeddings: List[List[float]],
+    distance_metric="cosine",
+) -> List[List]:
+    """
+    Return the distances between a query embedding and a list of embeddings.
+    """
+    distance_metrics = {
+        "cosine": spatial.distance.cosine,
+        "L1": spatial.distance.cityblock,
+        "L2": spatial.distance.euclidean,
+        "Linf": spatial.distance.chebyshev,
+    }
+    distances = [
+        distance_metrics[distance_metric](query_embedding, embedding)
+        for embedding in embeddings
+    ]
+    return distances
